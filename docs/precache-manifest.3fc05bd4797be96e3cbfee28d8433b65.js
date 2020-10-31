88be684dee155c3f24e303ecb59e80bad5b51f6b
Leonidios <programertv633@gmail.com>
2020-10-31 16:15:15 +0200

    Configured docs/ folder

---
+++ docs/precache-manifest.3fc05bd4797be96e3cbfee28d8433b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b760e7ff0fefa9f855c94d6a31ea03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d01a76f896285a8e5bd",
+    "url": "./static/css/main.738fa078.chunk.css"
+  },
+  {
+    "revision": "d51bcf0e36d76e5fe24a",
+    "url": "./static/js/2.9418427a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9418427a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d01a76f896285a8e5bd",
+    "url": "./static/js/main.d0c5691f.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
